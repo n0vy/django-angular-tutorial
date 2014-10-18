2837a7e7c3a3d86ac1a660b290b938193d135332
James Brewer <james@jamesbrewer.io>
2014-10-18 12:10:17 -0700

    Add STATICFILES_DIRS and TEMPLATE_DIRS settings.

--- django_angular_tutorial/settings.py
+++ django_angular_tutorial/settings.py
@@ -81,3 +81,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    'static',
+)
+
+TEMPLATE_DIRS = (
+    'templates',
+)
