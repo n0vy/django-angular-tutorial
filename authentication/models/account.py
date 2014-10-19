6bcb511e8ef50b364954a78eadc93a8f408d0763
James Brewer <james@jamesbrewer.io>
2014-10-18 17:38:30 -0700

    Add post_save and pre_delete hooks to Account model to ensure one-to-one relationship with User model.

--- authentication/models/account.py
+++ authentication/models/account.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save, pre_delete
+from django.dispatch import receiver
 
 
 class Account(models.Model):
@@ -10,3 +12,14 @@ class Account(models.Model):
 
     def __unicode__(self):
         return self.user.username
+
+    @receiver(post_save, sender=User)
+    def create_account_for_user(sender, instance=None, created=False, **kwargs):
+        if created:
+            Account.objects.get_or_create(user=instance)
+
+    @receiver(pre_delete, sender=User)
+    def delete_account_for_user(sender, instance=None, **kwargs):
+        if instance:
+            account = Account.objects.get(user_id=instance.id)
+            account.delete()
