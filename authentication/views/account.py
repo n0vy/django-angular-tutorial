5ec102a3179d4c53c809de0dc51b1936a4120a4d
James Brewer <james@jamesbrewer.io>
2014-10-18 18:08:46 -0700

    Only allow .list(), .retrieve(), and .update() methods on Account endpoints.

--- authentication/views/account.py
+++ authentication/views/account.py
@@ -1,12 +1,20 @@
-from rest_framework import viewsets
+from rest_framework import mixins
 from rest_framework import permissions
+from rest_framework import viewsets
 
 from authentication.models.account import Account
 from authentication.permissions import IsAccountOwnerOrStaff
 from authentication.serializers.account import AccountSerializer
 
 
-class AccountViewSet(viewsets.ModelViewSet):
+class ListRetrieveUpdateViewSet(mixins.ListModelMixin,
+                                mixins.RetrieveModelMixin,
+                                mixins.UpdateModelMixin,
+                                viewsets.GenericViewSet):
+    pass
+
+
+class AccountViewSet(ListRetrieveUpdateViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
 
