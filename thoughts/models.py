42662c4a21b3e3c6ce078e7ce4aefd0c300b5d5e
James Brewer <james@jamesbrewer.io>
2014-10-18 18:19:02 -0700

    Create Thought model.

--- thoughts/models.py
+++ thoughts/models.py
@@ -1,3 +1,12 @@
+from django import forms
 from django.db import models
 
-# Create your models here.
+from authentication.models.account import Account
+
+
+class Thought(models.Model):
+    author = models.ForeignKey(Account)
+    content = models.TextField()
+
+    def __unicode__(self):
+        return '{0} thinks "{1}"'.format(self.author.user.username, self.content)
