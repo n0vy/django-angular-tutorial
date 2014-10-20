cbee2bede981502bc760f80a557deec23ba2580a
James Brewer <james@jamesbrewer.io>
2014-10-20 16:23:28 -0700

    Calculate number of columns based on the width of the window.

--- static/javascripts/thoughts/controllers/thoughts.controller.js
+++ static/javascripts/thoughts/controllers/thoughts.controller.js
@@ -1,6 +1,7 @@
 angular.module('borg.thoughts.controllers')
   .controller('ThoughtsController', function ($scope) {
     $scope.nodes = [];
+    $scope.columns = 0;
 
     var getSmallestNode = function () {
       var heights = $scope.nodes.map(function (node) {
@@ -12,26 +13,45 @@ angular.module('borg.thoughts.controllers')
       return heights.indexOf(Math.min.apply(this, heights));
     };
 
-    var createColumns = function (thoughts) {
+    var calculateNumberOfColumns = function () {
+      var width = $(window).width();
+
+      if (width >= 1200) {
+        $scope.columns = 4;
+      } else if (width >= 992) {
+        $scope.columns = 3;
+      } else if (width >= 768) {
+        $scope.columns = 2;
+      } else {
+        $scope.columns = 1;
+      }
+    };
+
+    var createColumns = function () {
+      calculateNumberOfColumns();
+
+      $scope.nodes = [];
+
+      for (var i = 0; i < $scope.columns; ++i) {
+        $scope.nodes.push([]);
+      }
+    };
+
+    var fillColumns = function (thoughts) {
       for (var i = 0; i < thoughts.length; ++i) {
         var node = getSmallestNode();
         $scope.nodes[node].push(thoughts[i]);
       }
     };
 
-
-    $scope.$watch(
-      function () { 
-        return $scope.thoughts; 
-      },
-      function (newValue, oldValue) {
-        if (newValue !== oldValue) {
-          $scope.nodes = [];
-
-          for (var i = 0; i < 4; ++i ) { $scope.nodes.push([]) }
-
-          createColumns(newValue);
-        }
+    var render = function (newValue, oldValue) {
+      if (newValue !== oldValue) {
+        createColumns();
+        fillColumns(newValue);
       }
-    );
+    };
+
+
+    $scope.$watch(function () { return $scope.thoughts; }, render);
+    $scope.$watch(function () { return $(window).width(); }, render);
   });
