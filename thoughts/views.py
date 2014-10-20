9b047ba96c328503489a53d9274907f0762ebf53
James Brewer <james@jamesbrewer.io>
2014-10-20 12:16:54 -0700

    Move pre_save method to correct view and sort Thoughts in -created_at order.

--- thoughts/views.py
+++ thoughts/views.py
@@ -1,20 +1,19 @@
 from rest_framework import generics
 
+from authentication.models.account import Account
 from thoughts.models import Thought
 from thoughts.serializers import ThoughtSerializer
 
 
 class ThoughtListCreateView(generics.ListCreateAPIView):
-    queryset = Thought.objects.all()
+    queryset = Thought.objects.order_by('-created_at')
     serializer_class = ThoughtSerializer
+
+    def pre_save(self, obj):
+        obj.author = Account.objects.get(user=self.request.user)
+        return super(ThoughtListCreateView, self).pre_save(obj)
 
 
 class ThoughtRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Thought.objects.all()
     serializer_class = ThoughtSerializer
-
-    def pre_save(self, obj):
-        self.author = self.request.user
-        return super(ThoughtRetrieveUpdateDestroyView, self).pre_save(obj)
-
-
