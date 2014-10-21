a4032fdd2cc5e95433e7dff3332a511ef8dc7008
James Brewer <james@jamesbrewer.io>
2014-10-21 14:36:33 -0700

    Update IsAccountOwnerOrStaff permission.

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -2,8 +2,5 @@ from rest_framework import permissions
 
 
 class IsAccountOwnerOrStaff(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return view.action == 'retrieve' or request.user.is_staff
-
     def has_object_permission(self, request, view, obj):
-        return obj == request.user or request.user.is_staff
+        return obj == request.user.account or request.user.is_staff
