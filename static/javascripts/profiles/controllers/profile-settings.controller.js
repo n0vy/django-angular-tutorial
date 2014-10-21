606772a65c77c3d60ae86cf403435375f9015af9
James Brewer <james@jamesbrewer.io>
2014-10-21 14:37:36 -0700

    Add update and destroy functionality to profile settings page.

--- static/javascripts/profiles/controllers/profile-settings.controller.js
+++ static/javascripts/profiles/controllers/profile-settings.controller.js
@@ -22,4 +22,46 @@ angular.module('borg.profiles.controllers')
         console.log(data);
       }
     );
+
+    $scope.update = function () {
+      var options = {
+        timeout: 3000
+      };
+
+      Profile.update($scope.account).then(
+        function (data, status, headers, config) {
+          options.content = 'Your profile has been updated.';
+
+          $.snackbar(options);
+        },
+        function (data, status, headers, config) {
+          console.log(data);
+          options.content = 'Error: ' + data.error;
+
+          $.snackbar(options);
+        }
+      );
+    };
+
+    $scope.destroy = function () {
+      var options = {
+        timeout: 3000
+      };
+
+      Profile.destroy($scope.account).then(
+        function (data, status, headers, config) {
+          delete $cookies.authenticatedUser;
+          window.location = '/';
+
+          options.content = 'You have disconnected from The Borg.';
+
+          $.snackbar(options);
+        },
+        function (data, status, headers, config) {
+          options.content = 'YOU MUST NOT DISCONNECT FROM THE BORG!';
+
+          $.snackbar(options);
+        }
+      );
+    };
   });
--- static/javascripts/profiles/services/profile.service.js
+++ static/javascripts/profiles/services/profile.service.js
@@ -1,9 +1,17 @@
 angular.module('borg.profiles.services')
   .service('Profile', function ($http) {
     var Profile = {
+      destroy: function (account) {
+        return $http.delete('/api/v1/users/' + account.user_id + '/');
+      },
+
       get: function (username) {
         return $http.get('/api/v1/accounts/' + username + '/');
       },
+
+      update: function (account) {
+        return $http.put('/api/v1/accounts/' + account.username + '/', account);
+      }
     };
 
     return Profile;
