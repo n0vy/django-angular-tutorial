bb39708394294329187786da31753881a844c5cc
James Brewer <james@jamesbrewer.io>
2014-10-21 13:41:06 -0700

    Also redirect awar from /+:username/settings if not logged in.

--- static/javascripts/profiles/controllers/profile-settings.controller.js
+++ static/javascripts/profiles/controllers/profile-settings.controller.js
@@ -1,9 +1,15 @@
 angular.module('borg.profiles.controllers')
   .controller('ProfileSettingsController', function ($location, $cookies, $routeParams, $scope, Profile) {
-    var authenticatedUser = JSON.parse($cookies.authenticatedUser);
-    var profileOwner = $routeParams.username.substr(1);
-
-    if (authenticatedUser.username !== profileOwner) {
+    // Redirect if not logged in.
+    if (!$cookies.authenticatedUser) {
       $location.path('/');
+    } else {
+      var authenticatedUser = JSON.parse($cookies.authenticatedUser);
+      var profileOwner = $routeParams.username.substr(1);
+
+      // Redirect if logged in, but not the owner of this profile.
+      if (authenticatedUser.username !== profileOwner) {
+        $location.path('/');
+      }
     }
   });
