6de8682081bb9a10c7cdf831ec9b054f01982e7b
James Brewer <james@jamesbrewer.io>
2014-10-21 13:38:07 -0700

    Redirect away from /+:username/settings if the authenticated user does not own this profile.

--- static/javascripts/profiles/controllers/profile-settings.controller.js
+++ static/javascripts/profiles/controllers/profile-settings.controller.js
@@ -1,3 +1,9 @@
 angular.module('borg.profiles.controllers')
-  .controller('ProfileSettingsController', function ($scope, Profile) {
+  .controller('ProfileSettingsController', function ($location, $cookies, $routeParams, $scope, Profile) {
+    var authenticatedUser = JSON.parse($cookies.authenticatedUser);
+    var profileOwner = $routeParams.username.substr(1);
+
+    if (authenticatedUser.username !== profileOwner) {
+      $location.path('/');
+    }
   });
