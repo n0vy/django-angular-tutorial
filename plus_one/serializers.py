9fa969874e57088ffc2e21e216c83be8925fdbba
James Brewer <james@jamesbrewer.io>
2014-10-23 15:18:12 -0700

    Remove plus_one app.

--- plus_one/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from plus_one.models import PlusOne
-
-
-class PlusOneSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PlusOne
-        fields = ('account_id', 'thought_id',)
