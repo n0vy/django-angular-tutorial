ccf9b4d9623ee8611c20c01e785e7e6f15166b25
James Brewer <james@jamesbrewer.io>
2014-10-24 13:58:44 -0700

    Show message when there are no thoughts. Update icon for add new thought button.

--- static/javascripts/static/controllers/borg.controller.js
+++ static/javascripts/static/controllers/borg.controller.js
@@ -1,7 +1,6 @@
 angular.module('borg.static.controllers')
   .controller('BorgController', function ($scope, Authentication, Snackbar, Thoughts) {
     $scope.authenticatedUser = Authentication.authenticatedUser();
-    $scope.thoughts = [];
 
     var getThoughts = function () {
       Thoughts.all().then(
@@ -9,7 +8,6 @@ angular.module('borg.static.controllers')
           $scope.thoughts = data.data;
         },
         function (data, status, headesr, config) {
-          $scope.thoughts = null;
           Snackbar.error(data.error);
         }
       );
