57e1fe928bdb69a861a1ddc11123eb939fdc84a0
James Brewer <james@jamesbrewer.io>
2014-10-23 17:12:32 -0700

    Add service for snackbars.

--- static/javascripts/utils/services/snackbar.service.js
+++ static/javascripts/utils/services/snackbar.service.js
@@ -1,7 +1,15 @@
 angular.module('borg.utils.services')
   .service('Snackbar', function () {
     var Snackbar = {
+      error: function (content, options) {
+        Snackbar.snackbar('Error: ' + content, options);
+      },
+
       show: function (content, options) {
+        Snackbar.snackbar(content, options);
+      },
+
+      snackbar: function (content, options) {
         options = _.extend({ timeout: 3000 }, options);
         options.content = content;
 
