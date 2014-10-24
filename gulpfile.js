f80b199c75359acda82561d063687317663f1cea
James Brewer <james@jamesbrewer.io>
2014-10-24 14:14:13 -0700

    Combine `annotate` and `uglify` Gulp tasks into `build` task for synchronous execution.

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,12 @@ var gulp = require('gulp');
 var ngAnnotate = require('gulp-ng-annotate');
 var uglify = require('gulp-uglify');
 
-gulp.task('default', ['annotate', 'uglify'], function () {
+gulp.task('default', ['build'], function () {
 });
 
-gulp.task('annotate', function () {
+gulp.task('build', function () {
   return gulp.src('static/javascripts/**/*.js')
     .pipe(ngAnnotate())
-    .pipe(gulp.dest('dist/static/javascripts/'));
-});
-
-gulp.task('uglify', function () {
-  return gulp.src('dist/javascripts/**/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('dist/static/javascripts/'));
 });
