f05ec8735c130ccc2b94d31cc6d6f22c7638ddc7
James Brewer <james@jamesbrewer.io>
2014-10-29 14:55:48 -0700

    Allow for updating user email.

--- authentication/serializers/account.py
+++ authentication/serializers/account.py
@@ -15,3 +15,12 @@ class AccountSerializer(serializers.ModelSerializer):
             'account_id', 'user_id', 'username', 'email', 'tagline',
             'created_at', 'updated_at'
         )
+
+    def restore_object(self, attrs, instance=None):
+        account = super(AccountSerializer, self).restore_object(attrs, instance)
+
+        if account:
+            account.user.email = attrs.get('user.email', account.user.email)
+            account.user.save()
+
+        return account
